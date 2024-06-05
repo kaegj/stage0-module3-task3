3ba7e0a0c78333948fe04af1691bd8476559f748
kaegan <kkiefer4@student.umgc.edu>
2024-06-05 15:37:36 +0900

    Submission

--- src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
+++ src/main/java/school/mjc/stage0/module3/task3/BasicVariablesInitialization.java
@@ -2,6 +2,8 @@ package school.mjc.stage0.module3.task3;
 
 public class BasicVariablesInitialization {
     public static void main(String[] args) {
-        //write code here
+        int numOne = 1;
+        int numTwo = 10;
+        int numThree = 100;
     }
 }
